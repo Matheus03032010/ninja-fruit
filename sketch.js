4d7a9c4bf71de6e1e07f3e1c59b0ac0fb6776b9f
edwedja <edwedja_lima@hotmail.com>
2022-05-04 11:58:16 -0300

    Sounds aula32

--- sketch.js
+++ sketch.js
@@ -20,14 +20,27 @@ var button
 var bunny;
 var blink,eat,sad;
 
+var bk_song;  // SOM DO BG
+var cut_sound;  // SOM CORTANTO A CORDA
+var sad_sound;  // SOM TRISTE
+var eating_sound; // SOM COMENDO
+var air;  /// SOM DO AR
+
 function preload(){
   bg_img = loadImage('background.png');
   food = loadImage('melon.png');
   rabbit = loadImage('Rabbit-01.png');
 
+  bk_song = loadSound('sound1.mp3');
+  sad_sound = loadSound("sad.wav")
+  cut_sound = loadSound('rope_cut.mp3');
+  eating_sound = loadSound('eating_sound.mp3');
+  air = loadSound('air.wav');
+
   blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
   eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
   sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+ 
   eat.looping=false
   sad.looping=false
 }
@@ -47,14 +60,17 @@ function setup() {
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
-    textSize(50)
+  textSize(50)
   imageMode(CENTER);
+
   button=createImg("cut_btn.png")
   button.position(220,30)
   button.size(50,50) 
   button.mouseClicked(cortar)
+
   blink.frameDelay=20
-  bunny = createSprite(230,620,100,100);
+  eat.frameDelay = 20;
+  bunny = createSprite(350,620,100,100);
   bunny.scale = 0.2;
 
   bunny.addAnimation('blinking',blink);  // começa com essa! 
@@ -65,41 +81,40 @@ function setup() {
 
 function draw() {
   background(51);
- 
-  image(bg_img,width/2,height/2,490,690);
-  if(fruit!=null){
-  image(food,fruit.position.x,fruit.position.y,70,70);
-  }
-  rope.show();
   Engine.update(engine);
+  image(bg_img,width/2,height/2,490,690);
+
+  if(fruit!=null){
+    image(food,fruit.position.x,fruit.position.y,70,70);
+  }
+
+  rope.show();
   ground.show();
   drawSprites()
+
   if(collision(fruit,bunny)){
-  bunny.changeAnimation("eating")
-  
+    bunny.changeAnimation("eating")
   }
-if(collision(fruit,ground.body)){
- bunny.changeAnimation("crying")
-}
+  if(collision(fruit,ground.body)){
+    bunny.changeAnimation("crying")
+    fruit = null
+  }
 }
 
 function cortar(){
   rope.break()
   fruit_con. detach()
 }
- function collision(bodie,sprite){
-   if(bodie!=null){
-   
-   
- var distancia = dist(bodie.position.x,bodie.position.y,sprite.position.x,sprite.position.y)
- if(distancia<=80){
- World.remove(world,bodie)
- fruit=null  
- return true
  
- }
- else{
- return false
- }
- }
- }
+function collision(bodie,sprite){
+  if(bodie!=null){
+    var distancia = dist(bodie.position.x,bodie.position.y,sprite.position.x,sprite.position.y)
+    if(distancia<=80){
+      World.remove(world,bodie)
+      fruit=null  
+      return true
+    }else{
+      return false
+    }
+  }
+}
