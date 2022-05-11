c486f2097abe4d32caf73a25ffdb4424b0cef026
edwedja <edwedja_lima@hotmail.com>
2022-05-11 00:30:28 -0300

    mais desafios

--- sketch.js
+++ sketch.js
@@ -10,13 +10,13 @@ const Composite = Matter.Composite;
 let engine;
 let world;
 var ground;
-var fruit,rope;
-var fruit_con;
+var fruit,rope, rope2, rope3;
+var fruit_con, fruit_con_2 ,fruit_con_3;
 
 var bg_img;
 var food;
 var rabbit;
-var button
+var button, button2, button3;
 var bunny;
 var blink,eat,sad;
 
@@ -43,23 +43,29 @@ function preload(){
   blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
   eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
   sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
- 
   eat.looping=false
   sad.looping=false
 }
 
 function setup() {
+  
   createCanvas(500,700);
+  
   frameRate(80);
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(200,690,600,20);
 
-  rope = new Rope(7,{x:245,y:30});
+  rope = new Rope(7,{x:55,y:30});
+  rope2 = new Rope(7,{x:370,y:40});
+  rope3 = new Rope(4,{x:400,y:225});
+
   fruit = Bodies.circle(300,300,20);
   Matter.Composite.add(rope.body,fruit);
 
   fruit_con = new Link(rope,fruit);
+  fruit_con_2 = new Link(rope2,fruit);
+  fruit_con_3 = new Link(rope3,fruit);
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -67,18 +73,29 @@ function setup() {
   imageMode(CENTER);
 
   button=createImg("cut_btn.png")
-  button.position(220,30)
+  button.position(50,30)
   button.size(50,50) 
   button.mouseClicked(cortar)
+
+  button2 = createImg('cut_btn.png');
+  button2.position(330,35);
+  button2.size(60,60);
+  button2.mouseClicked(cortar2);
+
+  button3 = createImg('cut_btn.png');
+  button3.position(360,200);
+  button3.size(60,60);
+  button3.mouseClicked(cortar3);
+
   balão=createImg("balloon.png")
   balão.position(50,295)
   balão.size(100,100)
   balão.mouseClicked(ar)
+
   mute=createImg("mute.png")
-  mute.position(100,30)
+  mute.position(430,30)
   mute.size(50,50)
   mute.mouseClicked(xiu)
-
 
   blink.frameDelay=20
   eat.frameDelay = 20;
@@ -88,7 +105,6 @@ function setup() {
   bunny.addAnimation('blinking',blink);  // começa com essa! 
   bunny.addAnimation('eating',eat);
   bunny.addAnimation('crying',sad);
-  
 }
 
 function draw() {
@@ -101,6 +117,9 @@ function draw() {
   }
 
   rope.show();
+  rope2.show();
+  rope3.show();
+
   ground.show();
   drawSprites()
 
@@ -108,28 +127,45 @@ function draw() {
     bunny.changeAnimation("eating")
     eating_sound.play()
   }
+
   if(collision(fruit,ground.body)){
     bunny.changeAnimation("crying")
     fruit = null
     sad_sound.play()
   }
 }
+
 function ar(){
-air.play()
-Matter.Body.applyForce(fruit,{x:0,y:0},{x:0.05,y:0.0})
+  air.play()
+  Matter.Body.applyForce(fruit,{x:0,y:0},{x:0.05,y:0.0})
 }
+
 function xiu(){
-if(bk_song.isPlaying()){
-  bk_song.stop()
-}else{
-  bk_song.play()
-}
+  if(bk_song.isPlaying()){
+    bk_song.stop()
+  }else{
+    bk_song.play()
+  }
 }
 
 function cortar(){
   rope.break()
   fruit_con. detach()
   cut_sound.play()
+}
+
+function cortar2(){
+  cut_sound.play();   
+  rope2.break();
+  fruit_con_2.detach();
+  fruit_con_2 = null; 
+}
+
+function cortar3(){
+  cut_sound.play();   
+  rope3.break();
+  fruit_con_3.detach();
+  fruit_con_3 = null; 
 }
  
 function collision(bodie,sprite){
